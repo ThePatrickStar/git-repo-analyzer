3b3912533506f48dbbf5757dce9266e578c53079
Your Name <you@example.com>
2017-12-10 21:02:18 +0800

    main and utils

---
+++ main.py
@@ -0,0 +1,17 @@
+from utils import *
+
+
+def parse_params():
+    pass
+
+
+def main():
+    parse_params()
+    warn("warn")
+    info("info")
+    danger("danger")
+    ok("ok")
+
+
+if __name__ == '__main__':
+    main()
